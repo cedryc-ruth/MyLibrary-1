09abc4d749d4cf1a85824563f9a4a6909f4d48fb
LMGAO <ludomarigliano@gmail.com>
2021-03-15 20:54:01 +0100

    Second step

--- src/MainApplication1.java
+++ src/MainApplication1.java
@@ -4,8 +4,11 @@ public class MainApplication1 {
 	public static void main(String[] args) {
 		String[] membres = {};
 		String[] livres = {};
+		int [] emprunts = {};
 		
 		System.out.println("Hello World");
+		System.out.println("2ème modif apportée !");
+		
 	}
 
 }
