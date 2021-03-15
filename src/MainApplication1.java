01bfeecdd965466dec4644fcc03667ab6fcd3544
cruth <ceruth@epfc.eu>
2021-03-15 20:53:04 +0100

    Structure du code

--- src/MainApplication1.java
+++ src/MainApplication1.java
@@ -2,10 +2,13 @@
 public class MainApplication1 {
 	
 	public static void main(String[] args) {
+		//Déclaration des données
 		String[] membres = {};
-		String[] livres = {};
+		String[][] livres = new String[2][3];
 		
 		System.out.println("Hello World");
+		
+		System.out.println("Fin du programme");
 	}
 
 }
